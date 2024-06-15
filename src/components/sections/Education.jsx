b24491086d22741ec5fec1e0734fc87146f50c5c
Anik Roy <anikroy.uiu@gmail.com>
2024-06-15 20:06:11 +0600

    changes

--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -5,30 +5,39 @@ import Image from '../layers/Image'
 const Education = () => {
   return (
     <>
-      <Container className="bg-[#0F1624]">
-        <h2 className='text-[#12F7D6] text-[64px] font-semibold flex justify-center pt-16'>Education
+      <Container className="bg-[#0F1624] pb-[100px]">
+        <h2 className='text-[#12F7D6] text-[64px] font-semibold flex justify-center p-16'>Education
         </h2>
-        <Container>
-            <div className='w-1/2 flex items-center '>
-                <Image className="!w-1/2" src="../../../../src/assets/UIU.png"/>
-                <div className='w-1/2 '>
-                    <h2>United International University</h2>
-                    <p>BS in Computer Science & Engineering</p>
-                    <p>2023-2027(Running)</p>
+        <div className="flex justify-between gap-x-6 px-10 font-normal text-2xl text-white ">
+           <div className='w-full'>
+                <div className=' w-full flex items-center pb-[80px]'>
+                    <Image className="!w-[55%] " src="../../../../src/assets/UIU.png"/>
+                    <div className='w-full '>
+                        <h2>United International University</h2>
+                        <p>BS in Computer Science & Engineering</p>
+                        <p>2023-2027 (Running)</p>
+                    </div>
                 </div>
-            </div>
-            <div className='w-1/2 flex items-center '>
-                <Image className="!w-1/2" src="../../../../src/assets/UIU.png"/>
-                <div className='w-1/2 '>
-                    <h2>United International University</h2>
-                    <p>BS in Computer Science & Engineering</p>
-                    <p>2023-2027(Running)</p>
+                <div className=' w-full flex '>
+                    <Image className="!w-[55%]" src="../../../../src/assets/MGBHS.png"/>
+                    <div className='w-full '>
+                        <h2>Motijheel Govt.Boys' High School</h2>
+                        <p>SSC, Science , CGPA: 5.00</p>
+                        <p>2014</p>
+                    </div>
                 </div>
-            </div>
-            <div>
-
-            </div>
-        </Container>
+           </div>
+           <div className='!w-full'>
+                <div className=' w-full flex   '>
+                    <Image className="!w-[42%]" src="../../../../src/assets/DCC.png"/>
+                    <div className='w-full  '>
+                        <h2>Dhaka City College</h2>
+                        <p>HSC, Science , CGPA: 4.67</p>
+                        <p>2016</p>
+                    </div>
+                </div>
+           </div>
+        </div>
       </Container>
     </>
   )
